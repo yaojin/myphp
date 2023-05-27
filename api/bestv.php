8f09bfa4d5916272bdf6f5607634e0aab91b0378
yaojin <fbdeyj@gmail.com>
2023-05-27 18:31:11 +0800

    Update bestv.php
    优化代码

--- api/bestv.php
+++ api/bestv.php
@@ -1,20 +1,24 @@
 <?php
 date_default_timezone_set("Asia/Shanghai");
+
 $channel = empty($_GET['id']) ? "cctv16hd4k/15000000" : trim($_GET['id']);
 $array = explode("/", $channel);
 $stream = "http://121.12.115.154/liveplay-kk.rtxapp.com/live/program/live/{$array[0]}/{$array[1]}/";
 $timestamp = substr(time(), 0, 9) - 7;
-$current = "#EXTM3U" . PHP_EOL;
-$current .= "#EXT-X-VERSION:3" . PHP_EOL;
-$current .= "#EXT-X-TARGETDURATION:3" . PHP_EOL;
-$current .= "#EXT-X-MEDIA-SEQUENCE:{$timestamp}" . PHP_EOL;
+
+$current = "#EXTM3U" . PHP_EOL
+         . "#EXT-X-VERSION:3" . PHP_EOL
+         . "#EXT-X-TARGETDURATION:3" . PHP_EOL
+         . "#EXT-X-MEDIA-SEQUENCE:{$timestamp}" . PHP_EOL;
+
 for ($i = 0; $i < 3; $i++) {
     $timematch = $timestamp . '0';
     $timefirst = date('YmdH', $timematch);
-    $current .= "#EXTINF:3," . PHP_EOL;
-    $current .= $stream . $timefirst . "/" . $timestamp . ".ts" . PHP_EOL;
-    $timestamp = $timestamp + 1;
+    $current .= "#EXTINF:3," . PHP_EOL
+             .  $stream . $timefirst . "/" . $timestamp . ".ts" . PHP_EOL;
+    $timestamp += 1;
 }
+
 header("Content-Type: application/vnd.apple.mpegurl");
 header("Content-Disposition: attachment; filename=playlist.m3u8");
 echo $current;
